66ac614988ecb7a59fcf897a74146b32ff7781da
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2016-01-16 02:49:35 +0000

    Github link

--- src/ie/gmit/sw/package-info.java
+++ src/ie/gmit/sw/package-info.java
@@ -1,5 +1,6 @@
 /**
  * Application entry points and global classes.
+ * @see <a href="https://github.com/johnmalcolm/Multi-Threaded-Cypher-Breaker" target="_blank">Github</a>
  */
 
 package ie.gmit.sw;
